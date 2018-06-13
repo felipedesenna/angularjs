7827db862536651e2f8754e47b0ea7558d3d2fb1
Felipe de Senna <lipe.senna@hotmail.com>
2018-06-12 23:14:13 -0300

    remoção de comentarios desnecessarios

--- Angular2/api/conexao.php
+++ Angular2/api/conexao.php
@@ -1,26 +1,4 @@
 <?php
-/*$host = 'localhost';
-$user = 'root';
-$pass = 'vertrigo';
-$banco = 'lista_telefonica';
-
-$conn = mysqli_connect($host, $user, $pass, $banco) or die(mysqli_error());
-
-$query = "SELECT * FROM contatos" or die(mysqli_error());
-
-$result = $conn->query($query);
-$response = array();
-
- while ($row = mysqli_fetch_array($result)) {
-     $json["id"] = $row[0];
-     $json["nome"] = $row[1];
-     $json["telefone"] = $row[2];
-     $json["data"] = $row[3];
-     $json["operadora"] = $row[4];
-     array_push($response, $json);
-}
-
-echo json_encode($response);*/
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
